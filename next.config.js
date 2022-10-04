9b38d6956a11af722774e6cdc53bd4cf0ea949fd
Margus Lehiste <margus@createcore.com>
2022-10-04 21:29:32 +0300

    my first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: true,
+  },
+  // i18n: {
+  //   locales: ['en', 'zh'],
+  //   defaultLocale: 'en',
+  //   localeDetection: false
+  // },
+}
+
+module.exports = nextConfig
